8157093a48fdab63c20588fb89955f90eeeb83c3
Born <wissanapong001@gmail.com>
2019-07-20 15:11:10 +0700

    [Created]: วร้างไฟล์ทดสอบฟังก์ชัน calculateSecounds,calculateMinutes,calculateHours ในไ ฟล์ date_calculate_test.go

---
+++ date_calculate/date_calculate_test.go
@@ -0,0 +1,52 @@
+package date_calculate
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_calculateSeconds_Input_startDate_5_2_1996_endDate_20_7_2019_Should_Be_740188800(t * testing.T)  {
+
+
+	expected := 740188800
+	startDate := time.Date(1996,2,5,0,0,0,0,time.UTC)
+	endDate :=  time.Date(2019,7,20,0,0,0,0,time.UTC)
+
+	actual :=  calculateSeconds(startDate,endDate)
+
+
+	if actual != expected {
+		t.Errorf("Expected %d but it got %d",expected,actual)
+	}
+
+}
+func Test_calculateMinutes_Input_startDate_5_2_1996_endDate_20_7_2019_Should_Be_12336480(t * testing.T)  {
+
+
+	expected := 12336480
+	startDate := time.Date(1996,2,5,0,0,0,0,time.UTC)
+	endDate :=  time.Date(2019,7,20,0,0,0,0,time.UTC)
+
+	actual :=  calculateMinutes(startDate,endDate)
+
+
+	if actual != expected {
+		t.Errorf("Expected %d but it got %d",expected,actual)
+	}
+
+}
+func Test_calculateHours_Input_startDate_5_2_1996_endDate_20_7_2019_Should_Be_205608(t * testing.T)  {
+
+
+	expected := 205608
+	startDate := time.Date(1996,2,5,0,0,0,0,time.UTC)
+	endDate :=  time.Date(2019,7,20,0,0,0,0,time.UTC)
+
+	actual :=  calculateHours(startDate,endDate)
+
+
+	if actual != expected {
+		t.Errorf("Expected %d but it got %d",expected,actual)
+	}
+
+}
