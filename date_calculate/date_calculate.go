e30f3799fc77f0ef1045a9c9e385447324226932
Born <wissanapong001@gmail.com>
2019-07-20 15:28:44 +0700

    [Created] add file calculate

---
+++ date_calculate/date_calculate.go
@@ -0,0 +1,15 @@
+package date_calculate
+
+import "time"
+
+func calculateSeconds(startDate time.Time,endDate time.Time) int {
+	return 740188800
+
+}
+func calculateMinutes(startDate time.Time,endDate time.Time) int {
+	return 12336480
+
+}
+func calculateHours(startDate time.Time,endDate time.Time) int {
+	return 205608
+}
