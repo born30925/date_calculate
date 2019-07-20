ca8a9973ea9dc3e6046e42219aeecfa0ae18e0b1
Born <wissanapong001@gmail.com>
2019-07-20 16:00:50 +0700

    [Edited]: Edit file date_calculate.go function calculateSeconds implement code

--- date_calculate/date_calculate.go
+++ date_calculate/date_calculate.go
@@ -3,7 +3,8 @@ package date_calculate
 import "time"
 
 func calculateSeconds(startDate time.Time,endDate time.Time) int {
-	return 740188800
+	seccondsDiff:= endDate.Sub(startDate).Seconds()
+	return int(seccondsDiff)+1440*60
 
 }
 func calculateMinutes(startDate time.Time,endDate time.Time) int {
